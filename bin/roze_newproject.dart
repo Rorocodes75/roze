ce0cab0aa411e6bcff5f90c628f8a663bb18063a
rorocodes75 <rozealotaibi@email.com>
2024-12-04 15:31:48 +0300

    add method

--- bin/roze_newproject.dart
+++ bin/roze_newproject.dart
@@ -1,6 +1,10 @@
 import 'package:roze_newproject/roze_newproject.dart' as roze_newproject;
 
 void main(List<String> arguments) {
- print("hello");
- 
+
+print(product(2, 3));
+
+}
+int product (int a , int b){
+    return a *b;
 }
